0d9e354cf0f586b8a51c3fe78773cbb916ec71c6
Taite Jernigan <taite5100@gmail.com>
2023-02-24 20:26:48 +0000

    Completed Coding Challenge Average

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,10 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        double sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return sum / nums.length;
     }
 }
